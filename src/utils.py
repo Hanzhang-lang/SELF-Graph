548fae41d6307b1561ef616dd10bb98de5ab386a
Ethanywdong <ydong2022@gmail.com>
2024-12-05 02:52:10 +0000

    first commit

---
+++ src/utils.py
@@ -0,0 +1,67 @@
+import argparse
+import glob
+import json
+import os
+import re
+import string
+from sklearn.metrics import precision_score
+
+
+
+
+def normalize(s: str) -> str:
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    s = s.lower()
+    exclude = set(string.punctuation)
+    s = "".join(char for char in s if char not in exclude)
+    s = re.sub(r"\b(a|an|the)\b", " ", s)
+    # remove <pad> token:
+    s = re.sub(r"\b(<pad>)\b", " ", s)
+    s = " ".join(s.split())
+    return s
+
+
+def match(s1: str, s2: str) -> bool:
+    s1 = normalize(s1)
+    s2 = normalize(s2)
+    return s2 in s1
+
+def eval_acc(prediction, answer):
+    matched = 0.
+    for a in answer:
+        if match(prediction, a):
+            matched += 1
+    return matched / len(answer)
+
+def eval_hit(prediction, answer):
+    for a in answer:
+        if match(prediction, a):
+            return 1
+    return 0
+
+def eval_f1(prediction, answer):
+    if len(prediction) == 0:
+        return 0, 0, 0
+    matched = 0
+    prediction_str = ' '.join(prediction)
+    for a in answer:
+        if match(prediction_str, a):
+            matched += 1
+    precision = matched / len(prediction)
+    recall = matched / len(answer)
+    if precision + recall == 0:
+        return 0, precision, recall
+    else:
+        return 2 * precision * recall / (precision + recall), precision, recall
+
+def extract_topk_prediction(prediction, k=-1):
+    results = {}
+    for p in prediction:
+        if p in results:
+            results[p] += 1
+        else:
+            results[p] = 1
+    if k > len(results) or k < 0:
+        k = len(results)
+    results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+    return [r[0] for r in results[:k]]
